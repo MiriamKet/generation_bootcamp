12a09405e0cedec7acd44f733821f2cf7a741793
MiriamKet <miriamgonzalez16181@gmail.com>
2022-04-28 15:51:38 -0500

    ciclos

---
+++ Java_script/ciclos/assets/js/ciclos.js
@@ -0,0 +1,43 @@
+/* ciclos*/
+//for 
+// for .. in
+//.forEach
+
+
+//imprimir la palabra "hola mundo" 10 veces
+//(inicializamos la variable; condición; modificador del contador)
+
+
+//for
+//iteración= una vuelta de cada ciclo
+for(let i=0; i<10; i++){
+console.log("hola mundo",i);
+debugger; //
+}
+
+
+
+//while
+let control = 1; //control
+
+while (control <= 5){ //condicion a observar
+    console.log("hola desde un ciclo while",control);
+    control ++;//modificador de la variable control
+}
+
+
+
+
+
+//do while
+
+let i = 1;
+
+do{
+console.log("hola desde un do.. while",i);
+i++;
+debugger
+}while(i <= 5)
+//primero se ejecuta y luego se evalua, esa es la diferencia entre el while y do while, al menos se aplica una vez el código
+
+
